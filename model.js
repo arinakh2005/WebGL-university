350bdc010a88c051ec2778d8ad1e0c4804933c2c
arinakh2005 <arinakholodnytska3@gmail.com>
2024-11-07 09:07:34 +0200

    Feat: Use facet weighted normals for lighting effect

--- model.js
+++ model.js
@@ -92,19 +92,69 @@ export class Model {
         return normals;
     }
 
+    /** Generates facet-weighted normals for drawing the corrugated sphere. **/
+    getFacetWeightedNormals(vertices, indices) {
+        const normals = new Float32Array(vertices.length);
+
+        for (let i = 0; i < indices.length; i += 3) {
+            const firstIndex = indices[i] * 3;
+            const secondIndex = indices[i + 1] * 3;
+            const thirdIndex = indices[i + 2] * 3;
+
+            const firstVertex = [vertices[firstIndex], vertices[firstIndex + 1], vertices[firstIndex + 2]];
+            const secondVertex = [vertices[secondIndex], vertices[secondIndex + 1], vertices[secondIndex + 2]];
+            const thirdVertex = [vertices[thirdIndex], vertices[thirdIndex + 1], vertices[thirdIndex + 2]];
+
+            const firstEdge = [secondVertex[0] - firstVertex[0], secondVertex[1] - firstVertex[1], secondVertex[2] - firstVertex[2]];
+            const secondEdge = [thirdVertex[0] - firstVertex[0], thirdVertex[1] - firstVertex[1], thirdVertex[2] - firstVertex[2]];
+
+            const normal = [
+                firstEdge[1] * secondEdge[2] - firstEdge[2] * secondEdge[1],
+                firstEdge[2] * secondEdge[0] - firstEdge[0] * secondEdge[2],
+                firstEdge[0] * secondEdge[1] - firstEdge[1] * secondEdge[0]
+            ];
+            const normalLength = Math.sqrt(normal[0] ** 2 + normal[1] ** 2 + normal[2] ** 2);
+
+            normals[firstIndex] += normal[0] * normalLength;
+            normals[firstIndex + 1] += normal[1] * normalLength;
+            normals[firstIndex + 2] += normal[2] * normalLength;
+
+            normals[secondIndex] += normal[0] * normalLength;
+            normals[secondIndex + 1] += normal[1] * normalLength;
+            normals[secondIndex + 2] += normal[2] * normalLength;
+
+            normals[thirdIndex] += normal[0] * normalLength;
+            normals[thirdIndex + 1] += normal[1] * normalLength;
+            normals[thirdIndex + 2] += normal[2] * normalLength;
+        }
+
+        for (let i = 0; i < normals.length; i += 3) {
+            const nx = normals[i];
+            const ny = normals[i + 1];
+            const nz = normals[i + 2];
+            const nLength = Math.sqrt(nx * nx + ny * ny + nz * nz);
+
+            normals[i] /= nLength;
+            normals[i + 1] /= nLength;
+            normals[i + 2] /= nLength;
+        }
+
+        return normals;
+    }
+
     /** Buffers the vertex data for U and V curves in the WebGL context. **/
     bufferData() {
         const vertices = this.getVertices();
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
         this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(vertices), this.gl.STATIC_DRAW);
 
-        const normals = this.getNormals();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.normalBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(normals), this.gl.STATIC_DRAW);
-
         const indices = this.getIndices();
         this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
         this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), this.gl.STATIC_DRAW);
+
+        const normals = this.getFacetWeightedNormals(vertices, indices);
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.normalBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(normals), this.gl.STATIC_DRAW);
     }
 
     /** Draws the corrugated sphere model using buffered vertex and index data. **/
